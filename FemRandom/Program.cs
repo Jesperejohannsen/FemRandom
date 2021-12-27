d72fdc8ebd98ea5e92bfb02a3ee7f432d3a478a0
Jesper Elgaard Johannsen <jesperejohannsen@gmail.com>
2021-12-27 18:50:18 +0100

    Add project files.

---
+++ FemRandom/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace RandomGenerator
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(random.Next(1,11));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
